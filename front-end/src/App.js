58ea51a063ddc51d7c0901a0d23c65a42969ec89
JOAO VITOR LUCINI <joao.lucini@cs.up.edu.br>
2025-06-11 21:26:29 -0300

    alteracao para dotnet 7

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import CadastroFilme from './pages/CadastroFilme';
-import EditarFilme from './pages/EditarFilme';
-import Generos from './pages/Generos';
-import './styles/global.css';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cadastro" element={<CadastroFilme />} />
-          <Route path="/editar/:id" element={<EditarFilme />} />
-          <Route path="/generos" element={<Generos />} />
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- front-end/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      © {new Date().getFullYear()} - Seu Projeto
-    </footer>
-  );
-}
-
-export default Footer;
